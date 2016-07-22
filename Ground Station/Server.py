08a2f1d698ec95a1485a1f2d89b1f456954f55d2
“Jack <Jack@Mac-Mini.local>
2016-07-22 19:35:19 +1000

    okay

--- Ground Station/Server.py
+++ Ground Station/Server.py
@@ -1,11 +1,10 @@
 import socket
 from errno import *
-from multiprocessing import Process, Manager
-from multiprocessing.queues import SimpleQueue
+from multiprocessing import Process, Manager, SimpleQueue
 
 class Server(Process):
     """ Handles receiving and sending """
-    
+
     def __init__(self, send_queue, recv_queue, port=9999):
         Process.__init__(self)
         self.send_queue = send_queue
@@ -32,17 +31,18 @@ class Server(Process):
                     self.recv_queue.put(connection.recv(int(connection.recv(4).decode())).decode())
                     print("Recieved message")
                 except Exception as error:
-                    # Remove error messages due to non-blocking
-                    if error.errno != EWOULDBLOCK: 
-                        # Remove duplicate errors
-                        if not error.errno == previous_error: 
-                            previous_error = error.errno              
-                            print(error)
-                    # Disconnect if connection is broken
-                    if error.errno == ECONNRESET or error.errno == ECONNABORTED:
-                        print("Connection removed")
-                        connection.shutdown(0)
-                        break
+                    if hasattr(error, 'errno'):
+                        # Remove error messages due to non-blocking
+                        if error.errno != EWOULDBLOCK:
+                            # Remove duplicate errors
+                            if not error.errno == previous_error:
+                                previous_error = error.errno
+                                print(error)
+                        # Disconnect if connection is broken
+                        if error.errno == ECONNRESET or error.errno == ECONNABORTED:
+                            print("Connection removed")
+                            connection.shutdown(0)
+                            break
 
 # Setup process stuff
 
